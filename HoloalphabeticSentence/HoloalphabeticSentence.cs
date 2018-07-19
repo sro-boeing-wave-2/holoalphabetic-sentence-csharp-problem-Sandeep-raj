f6324228898ab197ee03ffc8f79a9d929d0b3117
Sandeep <gurusandeepraj@gmail.com>
2018-07-19 09:18:31 +0530

    First Commit

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,6 +8,10 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
+            if(input.ToLower().Where(c => alphabets.Contains(c)).GroupBy(c => c).Count() == 26)
+            {
+                return true;
+            }
             return false;
         }
     }
