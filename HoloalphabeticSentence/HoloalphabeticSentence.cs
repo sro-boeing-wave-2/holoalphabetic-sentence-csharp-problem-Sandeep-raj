c85ff2b599a92ac05e2a695e5f0b8f4f294fc5e5
Sandeep <gurusandeepraj@gmail.com>
2018-07-25 07:58:10 +0530

    Used distinct instead of group by

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,7 +8,7 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            if(input.ToLower().Where(c => alphabets.Contains(c)).GroupBy(c => c).Count() == 26)
+            if(input.ToLower().Where(c => alphabets.Contains(c)).Distinct().Count() == 26)
             {
                 return true;
             }
